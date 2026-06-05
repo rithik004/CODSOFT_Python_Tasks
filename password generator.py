9e9fec7ef57ce84233b81008912f514358fd4039
Rithikkumar <131897478+rithik004@users.noreply.github.com>
2026-06-05 16:45:31 +0530

    Added password length validation

--- password generator.py
+++ password generator.py
@@ -2,11 +2,18 @@ import random
 import string
 
 def generate_password():
-    length = int(input("Enter password length: "))
-    
-    characters = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(characters) for _ in range(length))
-    
-    print("Generated Password:", password)
+    try:    # Handle invalid inputs (non-integer values)
+        length = int(input("Enter password length: "))
+
+        if length < 4:
+            print("Password length should be at least 4.\n")
+            return
+
+        characters = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(random.choice(characters) for _ in range(length))
+        print("Generated Password:", password)
+
+    except ValueError:    # Handle non-integer inputs
+        print("Please enter a valid number.")
 
 generate_password()
